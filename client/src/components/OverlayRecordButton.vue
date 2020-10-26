834dddc8efd062f05c37fb835b1ed4eb47d79f27
Max Ripper <max@jovo.tech>
2020-10-26 13:10:24 +0100

    :recycle: Refactor record-button to initialize when holding for the first time

--- client/src/components/OverlayRecordButton.vue
+++ client/src/components/OverlayRecordButton.vue
@@ -3,17 +3,15 @@
     class="bg-gray-300 rounded-full p-8 focus:outline-none shadow-inset"
     @mousedown="onMouseDown"
     @touchstart="onMouseDown"
-    @click="onClick"
   >
-    <play-icon v-if="!$client.isInitialized" />
-    <mic-icon v-else :class="[$client.isRecordingInput ? 'text-red-600' : '']" />
+    <mic-icon :class="[$client.isRecordingInput ? 'text-red-600' : '']" />
   </button>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
 import MicIcon from 'vue-feather-icons/icons/MicIcon';
 import PlayIcon from 'vue-feather-icons/icons/PlayIcon';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   name: 'overlay-record-button',
@@ -23,21 +21,11 @@ import PlayIcon from 'vue-feather-icons/icons/PlayIcon';
   },
 })
 export default class OverlayRecordButton extends Vue {
-  async onClick() {
-    if (!this.$client.isInitialized) {
-      return this.$client.initialize();
-    }
-  }
-
-  // Could be critical for other OS than Windows because those try to get the MediaStream when starting to record.
-  // This will cause the mouse up event to be called before the actual recording has started,
-  // which will then cause the recording to not be stopped on immediate mouse up
-  // TODO find a solution for the issue mentioned above
   async onMouseDown(event: MouseEvent | TouchEvent) {
-    if (this.$client.isInitialized) {
-      event.preventDefault();
+    if (!this.$client.isInitialized) {
+      await this.$client.initialize();
     }
-    if (this.$client.isRecordingInput || !this.$client.isInitialized) {
+    if (this.$client.isRecordingInput) {
       return;
     }
     if (event instanceof MouseEvent) {
