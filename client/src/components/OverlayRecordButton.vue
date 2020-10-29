1204a14dab5b304c8d60059b9ea899eabfa7bc7e
aswetlow <a.swetlow@gmail.com>
2020-10-29 10:37:12 +0100

    :recycle: Update overlay starter

--- client/src/components/OverlayRecordButton.vue
+++ client/src/components/OverlayRecordButton.vue
@@ -1,11 +1,15 @@
 <template>
-  <button
-    class="bg-gray-100 dark:bg-gray-800 rounded-full p-8 focus:outline-none shadow-2xl"
-    @mousedown="onMouseDown"
-    @touchstart="onMouseDown"
-  >
-    <mic-icon class="text-gray-800 dark:text-gray-400" :class="[$client.isRecordingInput ? 'text-red-600 dark:text-red-600' : '']" />
-  </button>
+  <div>
+    <button
+      ref="recordButton"
+      class="bg-gray-100 dark:bg-gray-800 rounded-full p-8 focus:outline-none shadow-2xl "
+      :class="[$client.isRecordingInput ? 'shadow-inner circle-ripple' : '']"
+      @mousedown="onMouseDown"
+      @touchstart="onMouseDown"
+    >
+      <mic-icon class="text-gray-800 dark:text-gray-400" />
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -31,12 +35,32 @@ export default class OverlayRecordButton extends Vue {
     } else {
       window.addEventListener('touchend', this.onMouseUp);
     }
+
     await this.$client.startInputRecording();
   }
 
-  private onMouseUp(event: MouseEvent | TouchEvent) {
+  private onMouseUp() {
     window.removeEventListener('mouseup', this.onMouseUp);
     this.$client.stopInputRecording();
   }
 }
 </script>
+<style lang="css">
+.ripple {
+  animation: ripple-animation 0.5s linear infinite;
+}
+@keyframes ripple-animation {
+  100% {
+    box-shadow: 0 0 0 0.6em rgba(67, 74, 101, 0.3),
+    0 0 0 0.35em rgba(67, 74, 101, 0.3),
+    0 0 0 0.125em rgba(67, 74, 101, 0.3),
+    0 0 0 0 rgba(67, 74, 101, 0.3);
+  }
+  0% {
+    box-shadow: 0 0 0 1em rgba(67, 74, 101, 0),
+    0 0 0 0.65em rgba(67, 74, 101, 0.3),
+    0 0 0 0.35em rgba(67, 74, 101, 0.3),
+    0 0 0 0.125em rgba(67, 74, 101, 0.3);
+  }
+}
+</style>
